f392914a0068f36f3b913f4389e2a60ea81a5757
Evgenii <eugene.pokalyuk@gmail.com>
2026-06-25 17:13:49 +0700

    ci: exclude docs/ from the library ESLint run

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 
 export default tseslint.config(
-  { ignores: ['dist', 'coverage', 'node_modules'] },
+  { ignores: ['dist', 'coverage', 'node_modules', 'docs'] },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
